136fb3e9dbc1aeb7e0a3d22c98e926f87f2e9510
Adam Dad <adam-dad@hotmail.co.uk>
2019-02-12 20:36:27 +0000

    chrome scrolling bug fix

--- src/components/SolarSystem.vue
+++ src/components/SolarSystem.vue
@@ -36,12 +36,12 @@
 </template>
 
 <style>
-html {
-  overflow-x:hidden;
-}
 body {
+  overflow-x:hidden;
   background-image: #ffffff; /* radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%); */
   background-size: cover;
+}
+html {
 }
 
 .system {
