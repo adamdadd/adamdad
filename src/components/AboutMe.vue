305af0f4cdfe156a8ef0b7dde96ad7509c651f91
Adam Dad <adam-dad@hotmail.co.uk>
2019-02-16 18:37:06 +0000

    update info

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,107 +1,68 @@
 <template>
     <div id="aboutme">
-    <h2 id="who">Who On Earth is <span class="name">Adam Dad</span>?</h2>
     <div id="square"></div>
-    <sunburst :data="data">
-
-  <!-- Add behaviors -->
-        <template slot-scope="{ nodes, actions }">
-            <highlightOnHover :nodes="nodes" :actions="actions" />
-            <zoomOnClick :nodes="nodes" :actions="actions" />
-        </template>
-
-  <!-- Add information to be displayed on top the graph -->
-        <nodeInfoDisplayer slot="top" slot-scope="{ nodes }" :current="nodes.mouseOver" :root="nodes.root" description="of visits begin with this sequence of pages" />
-
-  <!-- Add bottom legend -->
-        <breadcrumbTrail slot="legend" slot-scope="{ nodes, colorGetter, width }" :current="nodes.mouseOver" :root="nodes.root" :colorGetter="colorGetter" :from="nodes.clicked" :width="width" />
-
-    </sunburst>
-    <img id="avatar" src="../assets/avatar.png" alt="Just Me">
-    <a href="/experience" style="text-decoration: none"><span class="buttonh" role="button">
+    <h2 id="who">Who On Earth is <span class="name">Adam Dad</span>?</h2>
+    <p>
+        I'm currently an Astrophysics student in my penultimate year of an MPhys degree.
+        <br />
+        Along the way I have worked on many projects, 
+        <br />
+        whether it's a 40% increase in student intake under my presidency 
+        <br />
+        or working on a project with Unilever,
+        <br />
+        I have had many interesting experiences along the way.
+    </p>
+    <aboutChart id="chart"/>
+    <div id="avatar">
+        <img src="../assets/avatar.png" alt="Just Me" />
+    </div>
+    <a href="/experience" style="text-decoration: none">
+        <span class="buttonh" role="button">
             <img src="../assets/launch.png" style="width: 32px"/>
-    </span></a>
+        </span>
+    </a>
     </div>
 </template>
 
 <script>
-import {
-  breadcrumbTrail,
-  highlightOnHover,
-  nodeInfoDisplayer,
-  sunburst,
-  zoomOnClick
-} from 'vue-d3-sunburst';
-import "vue-d3-sunburst/dist/vue-d3-sunburst.css";
+import aboutChart from './aboutChart.vue'
 
 export default {
     name: "AboutMe",
     components: {
-    breadcrumbTrail,
-    highlightOnHover,
-    nodeInfoDisplayer,
-    sunburst,
-    zoomOnClick
-  },
-  data() {
-    return {
-      tree:  {
-      "name": "flare",
-        "children": [
-          {
-            "name": "analytics",
-            "children": [
-              {
-                "name": "cluster",
-                "children": [
-                  { "name": "AgglomerativeCluster", "size": 3938 },
-                  { "name": "CommunityStructure", "size": 3812 },
-                  { "name": "HierarchicalCluster", "size": 6714 },
-                  { "name": "MergeEdge", "size": 743 }
-                ]
-              },
-              {
-                "name": "graph",
-                "children": [
-                  { "name": "BetweennessCentrality", "size": 3534 },
-                  { "name": "LinkDistance", "size": 5731 },
-                  { "name": "MaxFlowMinCut", "size": 7840 },
-                  { "name": "ShortestPaths", "size": 5914 },
-                  { "name": "SpanningTree", "size": 3416 }
-                ]
-              },
-              {
-                "name": "optimization",
-                "children": [
-                  { "name": "AspectRatioBanker", "size": 7074 }
-                ]
-              }
-            ]
-          }
-        ]
-      }
+        aboutChart
     }
-  }
 }
 </script>
 
 <style scoped>
-#square {
+#chart {
     display: inline-block;
+    position: absolute;
+    width: 25vw;
+    min-width: 200px;
+    max-width: 300px;
+    bottom: 2vw;
+    right: 0vw;
+    animation: fadeLeft 2s;
+    -moz-animation: fadeLeft 2s; /* Firefox */
+    -webkit-animation: fadeLeft 2s; /* Safari and Chrome */
+    -o-animation: fadeLeft 2s; /* Opera */
+}
+#square {
+    display: flex;
     position: absolute;
     background-color: #000000;
     opacity: 0.2;
     top: 0px;
     left: 0px;
-    padding: 500px 1000px 100vw;
+    padding: 500px 500px;
+    width: 45vw;
     transition: ease-in 4s;
     z-index: -1;
 }
 #who {
-    animation: fade 2s;
-    -moz-animation: fade 2s; /* Firefox */
-    -webkit-animation: fade 2s; /* Safari and Chrome */
-    -o-animation: fade 2s; /* Opera */
     font-family: 'Merriweather', serif;
     position: absolute;
     top: 10vw;
@@ -110,30 +71,43 @@ export default {
     text-align: left;
     font-size: 36px;
     color: #000000;
+    animation: fadeRight 2s;
+    -moz-animation: fadeRight 2s; /* Firefox */
+    -webkit-animation: fadeRight 2s; /* Safari and Chrome */
+    -o-animation: fadeRight 2s; /* Opera */
 }
 .name {
     opacity: 0.4 /*#ff4e0f*/;
+    animation: fadeRight 2s;
+    -moz-animation: fadeRight 2s; /* Firefox */
+    -webkit-animation: fadeRight 2s; /* Safari and Chrome */
+    -o-animation: fadeRight 2s; /* Opera */
 }
 p {
     position: relative;
     text-align: left;
-    font-size: 18px;
+    font-size: 22px;
+    word-wrap: break-word;
     top: 200px;
     left: 5vw;
-    width: 80vw;
-    color: #000000;
-    animation: fade 1s;
-    -moz-animation: fade 1s; /* Firefox */
-    -webkit-animation: fade 1s; /* Safari and Chrome */
-    -o-animation: fade 1s; /* Opera */
+    width: 60vw;
+    color: #ffffff;
+    background-color: #000000;
+    opacity: 0.8;
+    animation: fadeRight 2s;
+    -moz-animation: fadeRight 2s; /* Firefox */
+    -webkit-animation: fadeRight 2s; /* Safari and Chrome */
+    -o-animation: fadeRight 2s; /* Opera */
 }
 #avatar {
     display: flex;
     position: absolute;
-    top: 30vw;
+    overflow: hidden;
+    top: 4vw;
     right: -10vw;
-    width: 30vw;
-    border-radius: 50%;
+    width: 25vw;
+    min-width: 200px;
+    border-radius: 100vw;
     animation: fadeLeft 2s;
     -moz-animation: fadeLeft 2s; /* Firefox */
     -webkit-animation: fadeLeft 2s; /* Safari and Chrome */
@@ -144,7 +118,7 @@ a {
 }
 .buttonh {
     position: relative;
-    top: 500px;
+    top: 320px;
     left: 0vw;
     display: inline-table;
     margin-left: auto;
@@ -205,6 +179,26 @@ a {
        opacity:1;
    }
 }
+@keyframes fadeLeft {
+   0% {
+       transform: translateX(20px);
+       opacity:0;
+   }
+   100% {
+       transform: translateX(0px);
+       opacity:1;
+   }
+}
+@-moz-keyframes fadeRight { /* Firefox */
+   0% {
+       transform: translateX(20px);
+       opacity:0;
+   }
+   100% {
+       transform: translateX(0px);
+       opacity:1;
+   }
+}
 @-webkit-keyframes fadeLeft { /* Safari and Chrome */
    0% {
        transform: translateX(20px);
@@ -218,6 +212,46 @@ a {
 @-o-keyframes fadeLeft { /* Opera */
    0% {
        transform: translateX(20px);
+       opacity:0;
+   }
+   100% {
+       transform: translateX(0px);
+       opacity:1;
+   }
+}
+@keyframes fadeRight {
+   0% {
+       transform: translateX(-20px);
+       opacity:0;
+   }
+   100% {
+       transform: translateX(0px);
+       opacity:1;
+   }
+}
+@-webkit-keyframes fadeRight {
+   0% {
+       transform: translateX(-20px);
+       opacity:0;
+   }
+   100% {
+       transform: translateX(0px);
+       opacity:1;
+   }
+}
+@-moz-keyframes fadeRight {
+   0% {
+       transform: translateX(-20px);
+       opacity:0;
+   }
+   100% {
+       transform: translateX(0px);
+       opacity:1;
+   }
+}
+@-o-keyframes fadeRight {
+   0% {
+       transform: translateX(-20px);
        opacity:0;
    }
    100% {
